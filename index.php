8e17e8f6d0ac9f1ba86ecc689782ad10015901af
Nodws <Nodws@users.noreply.github.com>
2017-06-13 16:05:46 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,13 @@
+<?
+/**
+ * @package    Nod.CMS.Core
+ *
+ * @copyright  Copyright (C) 2014 - 2018 Nodws.com. All rights reserved.
+ * @license    MIT License; see LICENSE file for details.
+ */
+
+ error_reporting(0);
+ header('X-Powered-By: InternetExplorer6');
+ include "core/engine.php";
+
+?>
